f9a4198fd07e590b9c17f6240ec35d29c786d00a
Максим Бондарчук <bondarcumk086@gmail.com>
2025-05-15 10:06:06 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => {
+          setPosts(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error('Помилка при отриманні даних:', error);
+          setLoading(false);
+        });
+    }, 1500);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Список постів</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        posts.map((post) => (
+          <div key={post.id} style={{ marginBottom: '20px' }}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <hr />
+          </div>
+        ))
+      )}
     </div>
   );
 }
